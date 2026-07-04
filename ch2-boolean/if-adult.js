7650d0d9d296e90957204cc6035ae717ac68f7e2
makisragkousis <makisragkousis@gmail.com>
2026-07-04 21:12:07 +0300

    fix: update age comparison to include the adult lower limit

--- ch2-boolean/if-adult.js
+++ ch2-boolean/if-adult.js
@@ -1,7 +1,7 @@
 const age = 17;
 const ADULT_LOWER_LIMIT = 18;
 
-if (age > ADULT_LOWER_LIMIT) {
+if (age >= ADULT_LOWER_LIMIT) {
     console.log("You may enter.")
 } else {
     console.log("Sorry adults only.")
