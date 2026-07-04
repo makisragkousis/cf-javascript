1a245f6889e1d0d68612400757df502812485d4b
makisragkousis <makisragkousis@gmail.com>
2026-07-04 20:48:59 +0300

    fix: correct pluralization in seconds output message

--- ch1-numbers/seconds.js
+++ ch1-numbers/seconds.js
@@ -6,4 +6,4 @@ const SEC_PER_MIN = 60;
 const mins = Math.floor(totalSeconds / SEC_PER_MIN) ;
 const secs = totalSeconds % SEC_PER_MIN;
 
-console.log(`${totalSeconds} seconds equals ${mins} minute(s) and ${secs} second(s)`);
+console.log(`${totalSeconds} second(s) equals ${mins} minute(s) and ${secs} second(s)`);
