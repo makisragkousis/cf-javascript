a72e7ad76ea5b6645709928cd4e4bdb8d392669a
makisragkousis <makisragkousis@gmail.com>
2026-07-09 19:03:46 +0300

    feat: add examples for boolean operations and ternary expressions

---
+++ ch2-boolean/ternary.js
@@ -0,0 +1,7 @@
+const athensTemp = 30;
+const lamiaTemp = 25;
+
+const diff = athensTemp - lamiaTemp;
+const absoluteDiff = diff < 0 ? -diff : diff;
+
+console.log(`Absolute Diff is ${absoluteDiff}`);
