bb5d0e0dfac6bdcc0ee9934b8573c1d27d0c252d
makisragkousis <makisragkousis@gmail.com>
2026-07-17 01:11:11 +0300

    fix: correct spacing in for loop in deleteBackwards function

--- ch04-arrays/delete-multiple-items.js
+++ ch04-arrays/delete-multiple-items.js
@@ -4,7 +4,7 @@ const deleteBackwards = (arr, val) => {
 
     if (!Array.isArray(arr)) return;
 
-    for (let i = arr.length -1; i >= 0; i--) {
+    for (let i = arr.length - 1; i >= 0; i--) {
         if (arr[i] === val) {
             arr.splice(i, 1);
         }
