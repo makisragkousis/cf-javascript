9a62d49340fb47c98c982de9c09ddfe57c545ee5
makisragkousis <makisragkousis@gmail.com>
2026-07-16 17:47:09 +0300

    fix: correct array validation in getMin and getMin2 functions

--- ch4-arrays/min-max-array.js
+++ ch4-arrays/min-max-array.js
@@ -1,7 +1,7 @@
 const ages = [20, 30, 50, 18, 34];
 
 function getMin(arr) {
-    if (!Array.isArray(ages) || arr.length === 0) return;
+    if (!Array.isArray(arr) || arr.length === 0) return;
 
     let minVal = Math.min(...arr);
     let minPosition = arr.indexOf(minVal);
@@ -9,7 +9,7 @@ function getMin(arr) {
 }
 
 function getMin2(...arr) {
-    if (!Array.isArray(ages) || arr.length === 0) return;
+    if (!Array.isArray(arr) || arr.length === 0) return;
 
     let minVal = Math.min(...arr);
     let minPosition = arr.indexOf(minVal);
