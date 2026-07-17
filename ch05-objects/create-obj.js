37a7c38f960a622e54ab59ee9fcff83f8932728b
makisragkousis <makisragkousis@gmail.com>
2026-07-17 19:43:09 +0300

    feat: add user and person objects with properties and methods for manipulation

---
+++ ch05-objects/create-obj.js
@@ -0,0 +1,14 @@
+const obj = {}                   // empty object
+
+const point = { x: 1, y: 2 };
+point.z = 4;
+
+const person = {
+    firstname: "Alice",
+    lastname: "W.",
+    hobbies: [teenis, footbal, piano],
+    home: { x: 234567.65, y: 334456.99 },
+    geometry: [{x: 23445, y: 1234.76}, {x: 123333.55, y:34567}, {x: 129333.55, y: 30067}],
+    active: true,
+    age: 45
+}
