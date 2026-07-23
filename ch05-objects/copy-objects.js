b9759a24e8bc5ebd44759af9846eabb915f8662e
makisragkousis <makisragkousis@gmail.com>
2026-07-23 18:41:01 +0300

    fix: update shallow copy example to demonstrate object reference behavior

--- ch05-objects/copy-objects.js
+++ ch05-objects/copy-objects.js
@@ -4,7 +4,16 @@ const initial = {
     address: {street: "Patission", streetNum: 80}
 }
 
+const copyObj = initial;
+copyObj.firstname = 'Bob';
+console.log(initial.firstname);   // Shallow Copy - Bob
+
 const copyInitial = {...initial};                                 // SHALLOW COPY
+copyInitial.firstname = 'Bob';
+copyInitial.address.street = 'Aristotelous';
+
+console.log(initial.firstname);
+console.log(initial.address.street);
 
 const copyInitial2 = JSON.parse(JSON.stringify(initial));         // DEEP COPY
 
