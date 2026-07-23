c479fbcf63f9ffd11e6179a0bb0024915dcf841c
makisragkousis <makisragkousis@gmail.com>
2026-07-23 19:44:23 +0300

    feat: add user object and enhance getPropertyValue function with optional chaining and existence checks

--- ch05-objects/review.js
+++ ch05-objects/review.js
@@ -1,3 +1,8 @@
+const user = {
+    id: 1,
+    firstname: 'Alice'
+}
+
 // Άσκηση 1
 // Μία συνάρτηση getPropertyValue(obj, propertyName)
 // που επιστρέφει την τιμή της ιδιότητας με αυτό
@@ -6,4 +11,37 @@
 function getPropertyValue(obj, propertyName) {
     // return obj.propertyName λάθος, θα αναμέναμε μέσα στο obj να υπάρχει "propertyName"
     return obj[propertyName];
+}
+
+console.log(getPropertyValue(user, "id"));
+
+// Άσκηση 1b
+// Μία συνάρτηση getPropertyValue(obj, propertyName)
+// που επιστρέφει την τιμή της ιδιότητας με αυτό
+// το όνομα και λαμβάνει υπόψη την περίπτωση το obj
+// να είναι null/undefined
+
+function getPropertyValue(obj, propertyName) {
+    // return obj.propertyName λάθος, θα αναμέναμε μέσα στο obj να υπάρχει "propertyName"
+    return obj?.[propertyName];       // optional chaining
+}
+
+console.log(getPropertyValue(null, "id"));
+
+
+// Άσκηση 1c
+// Μία συνάρτηση getPropertyValue(obj, propertyName)
+// που επιστρέφει την τιμή της ιδιότητας με αυτό
+// το όνομα και λαμβάνει υπόψη την περίπτωση το obj
+// να είναι null/undefined ή και το propertyName να μην
+// υπάρχει
+
+function getPropertyValue(obj, propertyName) {
+    if (obj == null || obj == undefined) {
+        return undefined;
+    }
+
+    return Object.hasOwn(obj, propertyName)
+        ? obj[propertyName]
+        : undefined
 }
