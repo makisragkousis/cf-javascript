005cd3c88b652257d861dc03f3d2bf8f38177b64
makisragkousis <makisragkousis@gmail.com>
2026-07-23 15:57:48 +0300

    fix: correct spelling errors in hobbies array

--- ch05-objects/create-obj.js
+++ ch05-objects/create-obj.js
@@ -6,7 +6,7 @@ point.z = 4;
 const person = {
     firstname: "Alice",
     lastname: "W.",
-    hobbies: [teenis, footbal, piano],
+    hobbies: [tennis, football, piano],
     home: { x: 234567.65, y: 334456.99 },
     geometry: [{x: 23445, y: 1234.76}, {x: 123333.55, y:34567}, {x: 129333.55, y: 30067}],
     active: true,
